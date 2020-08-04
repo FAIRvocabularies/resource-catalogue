615cee1e09afacc9cd6263639b3bea4a188aa202
nut-hatch <niklas.kolbe@googlemail.com>
2020-08-04 18:17:44 +0200

    switch to jekyll collections with custom github action and python script to build the site, required for individual pages of yml files in _data/resources

---
+++ build/buildResourceCollection.py
@@ -0,0 +1,26 @@
+import os
+
+def main():
+    directory = "_data/resources"
+    for filename in os.listdir(directory):
+        if filename.endswith(".yml"):
+            #print(os.path.join(directory, filename))
+            resourceFile = open(os.path.join(directory, filename), "r")
+            if resourceFile.mode == 'r':
+                 contents = resourceFile.read()
+                 #print contents
+                 if not os.path.exists("_resources/"):
+                    os.makedirs("_resources/")
+                 collectionFile = open("_resources/"+os.path.splitext(filename)[0]+".md", "w")
+                 collectionFile.write("---\n")
+                 collectionFile.write(contents+"\n")
+                 collectionFile.write("layout: resource\n")
+                 collectionFile.write("---\n")
+                 collectionFile.close()
+                 resourceFile
+            continue
+        else:
+            continue
+
+if __name__== "__main__":
+  main()
